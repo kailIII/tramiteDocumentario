2e0fab1feb36628963421fe65e4f296344916beb
Benjacho <christianbfc97@gmail.com>
2015-12-28 00:27:56 -0500

    Imprime la columna n_usu de la tabla usuario

--- conexion.php
+++ conexion.php
@@ -3,4 +3,15 @@ $mysqli = mysqli_connect("localhost","root","","tramite");
 if(mysqli_connect_errno($mysqli)){
 	echo "Fallo al conectar a MySQL: ".mysql_connect_error();
 }
+
+$consulta = "SELECT n_usu FROM usuario";
+if($resultado = $mysqli->query($consulta)){
+	while($fila = $resultado->fetch_assoc()){
+		printf("%s \n", $fila["n_usu"]);
+	}
+
+	$resultado->free();
+}
+
+$mysqli->close();
 ?>
